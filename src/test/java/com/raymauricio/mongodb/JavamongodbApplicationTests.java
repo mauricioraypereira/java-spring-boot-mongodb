8986e01e4d86b7133ffe74597fa2caa423d0abad
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-10 16:04:52 -0300

    Creating project

---
+++ src/test/java/com/raymauricio/mongodb/JavamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymauricio.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
