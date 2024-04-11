fe6b48abad2b5815ee2dbb270eb51d0b4e80c31d
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-11 17:09:19 -0300

    Finding user by id + exceptions

--- src/main/java/com/raymauricio/mongodb/resources/UserResource.java
+++ src/main/java/com/raymauricio/mongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +29,9 @@ public class UserResource {
 		return ResponseEntity.ok().body(userListDTO);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User userFounded = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(userFounded));
+	}
 }
--- src/main/java/com/raymauricio/mongodb/services/UserService.java
+++ src/main/java/com/raymauricio/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.raymauricio.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raymauricio.mongodb.domain.User;
 import com.raymauricio.mongodb.repositories.UserRepository;
+import com.raymauricio.mongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Usuário não foi encontrado."));
+	}
 }
