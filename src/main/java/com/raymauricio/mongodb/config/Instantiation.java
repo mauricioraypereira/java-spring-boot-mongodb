149d8ba7ad599285860cf3828c07496bbd38de2f
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-11 20:00:57 -0300

    Creating entity post with a nested user

--- src/main/java/com/raymauricio/mongodb/config/Instantiation.java
+++ src/main/java/com/raymauricio/mongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.raymauricio.mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.raymauricio.mongodb.domain.Post;
 import com.raymauricio.mongodb.domain.User;
+import com.raymauricio.mongodb.repositories.PostRepository;
 import com.raymauricio.mongodb.repositories.UserRepository;
 
 @Configuration
@@ -15,15 +19,26 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para Rio grande do Sul, Abraços!", u1);
+		Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei da aqui no RS!", u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
