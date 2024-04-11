f0ceec30d72253e91fbdbe2d9431aa2c12e89ac7
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-11 16:49:31 -0300

    Data instanciation on mongodb using Instantiation class

---
+++ src/main/java/com/raymauricio/mongodb/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.raymauricio.mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.raymauricio.mongodb.domain.User;
+import com.raymauricio.mongodb.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com");
+		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+	}
+}
