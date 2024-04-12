4362eb710b3eda57ec7a153d001e54050fdd88f3
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-12 09:16:25 -0300

    Add comments to posts

--- src/main/java/com/raymauricio/mongodb/config/Instantiation.java
+++ src/main/java/com/raymauricio/mongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.raymauricio.mongodb.domain.Post;
 import com.raymauricio.mongodb.domain.User;
 import com.raymauricio.mongodb.dto.AuthorDTO;
+import com.raymauricio.mongodb.dto.CommentDTO;
 import com.raymauricio.mongodb.repositories.PostRepository;
 import com.raymauricio.mongodb.repositories.UserRepository;
 
@@ -38,8 +39,29 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para Rio grande do Sul, Abraços!", new AuthorDTO(u1));
-		Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei da aqui no RS!", new AuthorDTO(u1));
+		Post post1 = new Post(null, 
+				sdf.parse("21/03/2024"), 
+				"Partiu viagem", 
+				"Vou viajar para Rio grande do Sul, Abraços!", 
+				new AuthorDTO(u1));
+		Post post2 = new Post(null, 
+				sdf.parse("23/03/2024"), 
+				"Bom dia", 
+				"Acordei da aqui no RS!", 
+				new AuthorDTO(u1));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!",
+				sdf.parse("21/03/2018"),
+				new AuthorDTO(u2));
+		CommentDTO c2 = new CommentDTO("Aproveite!",
+				sdf.parse("22/03/2018"),
+				new AuthorDTO(u3));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",
+				sdf.parse("23/03/2018"),
+				new AuthorDTO(u2));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
