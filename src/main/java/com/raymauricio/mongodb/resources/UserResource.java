cf5001dddb12f859c6f2e52db7bb2ff359eed1c1
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-12 09:01:46 -0300

    Endpoint to return all posts from user by id

--- src/main/java/com/raymauricio/mongodb/resources/UserResource.java
+++ src/main/java/com/raymauricio/mongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.raymauricio.mongodb.domain.Post;
 import com.raymauricio.mongodb.domain.User;
 import com.raymauricio.mongodb.dto.UserDTO;
 import com.raymauricio.mongodb.services.UserService;
@@ -62,5 +63,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User userFounded = userService.findById(id);
+		return ResponseEntity.ok().body(userFounded.getPosts());
 	}
 }
