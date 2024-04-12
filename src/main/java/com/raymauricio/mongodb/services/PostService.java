eded8fc6044d4eb2bdef3c5d8e348ba2ec99db3d
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-12 09:29:48 -0300

    Simple query using query methods from spring-data-mongodb

--- src/main/java/com/raymauricio/mongodb/services/PostService.java
+++ src/main/java/com/raymauricio/mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.raymauricio.mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> 
 			new ObjectNotFoundException("Postagem não foi encontrada."));
+	}
+	
+	public List<Post> findByTitle(String title) {
+		return postRepository.findByTitleContainingIgnoreCase(title);
 	}
 }
