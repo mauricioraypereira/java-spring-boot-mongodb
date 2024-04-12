8e40a7d8f685330e94d222d47eed3b16af7ea19f
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-12 09:07:57 -0300

    Getting post by id

---
+++ src/main/java/com/raymauricio/mongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.raymauricio.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raymauricio.mongodb.domain.Post;
+import com.raymauricio.mongodb.repositories.PostRepository;
+import com.raymauricio.mongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Postagem não foi encontrada."));
+	}
+}
